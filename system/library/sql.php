cf4a5fdfa713a06f8905c353dfc6fc90a09034c0
xiaoai <joaner1206@gmail.com>
2013-04-02 19:21:40 +0800

    add sql create

--- system/library/sql.php
+++ system/library/sql.php
@@ -1,6 +1,50 @@
 <?php
 namespace sys\library;
 
+use system\super\library;
 
-final class 
+final class sql implements library 
+{
+	public static $prefix = '';
+
+	public $link = '';
+	
+	public function __call($name, $value)
+	{
+		$value = array_shift($value);
+		$this->{$name} = call_user_func(array($this, $name), $value);
+	}
+
+	protected function select($field='*')
+	{
+		return is_array($field) ? '`'. implode('`, `', $field) .'`' : $field;
+	}
+	
+	protected function from($table)
+	{
+		return '`'.self::$prefix.$table.'`';
+	}
+	
+	protected function where($filed, $value, $do='=')
+	{
+		$where = $this->link;
+		switch($do){
+			case '=':
+			case '<':
+			case '>':
+			case '>=':
+			case '<=':
+				$where .= "{$filed} {$do} '{$value}'";
+			break;
+			case 'LIKE':
+				$where .= "{$filed} LIKE '%{$value}%'";
+			break;
+			case 'IS':
+			
+		}
+		
+		$this->link || ($this->link='AND');
+	}
+	
+}
 
